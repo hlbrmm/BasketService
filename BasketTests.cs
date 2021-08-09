a0b537fbe52c69369ad392ebb46d52ae5eb8f2e6
Halil Eren <hlbrmm@gmail.com>
2021-08-09 03:31:48 +0300

    GenerateProgramFile => 2main class çözümü, test class eklendi.

---
+++ BasketTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace BasketService
+{
+    public class BasketTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Add_To_Basket_()
+        {
+            //Arrange
+            //Act
+            //Assert
+        }
+    }
+}
